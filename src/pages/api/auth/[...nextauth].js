483456efa66604a877d008eaaa310aa29e0e7dc0
ManassesM <mouramana6@gmail.com>
2022-04-16 12:22:22 -0300

    feat: mongodb adicionado

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,7 +3,11 @@ import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import FacebookProvider from 'next-auth/providers/facebook'
 
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from './lib/mongodb'
+
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   session: {
     jwt: true
   },
